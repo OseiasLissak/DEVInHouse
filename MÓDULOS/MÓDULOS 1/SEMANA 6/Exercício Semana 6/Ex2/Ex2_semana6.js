feadb12efeb3d96caf87d24f56d3a3567c7cb1b5
Lissak dos Santos Oseias <LissakdosSantosOseias@JohnDeere.com>
2022-04-07 16:01:58 -0300

    atualização ex2

--- MÓDULOS/MÓDULOS 1/SEMANA 6/Exercício Semana 6/Ex2/Ex2_semana6.js
+++ MÓDULOS/MÓDULOS 1/SEMANA 6/Exercício Semana 6/Ex2/Ex2_semana6.js
@@ -1,16 +1,22 @@
 function calcular(numero1, operacao, numero2){
     
-    numero1 = document.getElementById("primeiro-num");
-    operacao = document.getElementById("tipo-operacao");
-    numero2 = document.getElementById("segundo-num");
+    numero1 = document.querySelector.value("#primeiro-num");
+    operacao = document.querySelector.value("#tipo-operacao");
+    numero2 = document.querySelector.value("#segundo-num");
+
+    console.log(numero1)
+
+    console.log(typeof(numero1.value))
+    console.log(typeof(operacao.value))
+    console.log(typeof(numero2.value))
     
     console.log(operacao)
     
     switch(operacao){
-        case '+':
-        resultado = numero1 + numero2;
-        document.createElement('p')
+        case '+':   
+        resultado = numero1.value + numero2.value;
         alert (resultado);
+
         break;
     
         case '-':
