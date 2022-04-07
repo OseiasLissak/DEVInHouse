343f13805857300fac7b295b06ab12acab502bdf
Oseias Lissak <oseiaslissak@hotmail.com>
2022-04-06 22:33:45 -0300

    06-04-22

---
+++ MÓDULOS/MÓDULOS 1/SEMANA 6/Exercício Semana 6/Ex1/ex1_semana6.js
@@ -0,0 +1,12 @@
+/* Crie uma página html e adicione um título h1 com a descrição "Título da página";
+Adicione um botão com a descrição "Alterar título" e adicione o evento de click nele para uma função javascript;
+Crie a função javascript para alterar o título, 
+nela deve ser buscado o elemento h1 e alterado o texto para "Título da página alterado";
+ */
+
+function alteraTitulo(){
+    document.getElementById('title');
+
+    
+
+}
