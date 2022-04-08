353ef13add94a9ab82f942d9c0e083d2fba126a2
Oseias Lissak <oseiaslissak@hotmail.com>
2022-04-07 23:42:55 -0300

    07-04-22, 23:44

--- MÓDULOS/MÓDULOS 1/SEMANA 6/Exercício Semana 6/Ex2/Ex2_semana6.js
+++ MÓDULOS/MÓDULOS 1/SEMANA 6/Exercício Semana 6/Ex2/Ex2_semana6.js
@@ -1,22 +1,28 @@
-function calcular(numero1, operacao, numero2){
+function calcular(){
     
-    numero1 = document.querySelector.value("#primeiro-num");
-    operacao = document.querySelector.value("#tipo-operacao");
-    numero2 = document.querySelector.value("#segundo-num");
+    numero1 = parseInt(document.getElementById('num1').value);
+    operacao = document.getElementById('operacao').value;
+    numero2 = parseInt(document.getElementById('num2').value);
 
+    console.log(typeof(numero1))
     console.log(numero1)
 
-    console.log(typeof(numero1.value))
-    console.log(typeof(operacao.value))
-    console.log(typeof(numero2.value))
-    
-    console.log(operacao)
+    console.log(typeof(operacao))
+    console.log((operacao))
+
+    console.log(typeof(numero2))
+    console.log(numero2)
+  
     
     switch(operacao){
-        case '+':   
-        resultado = numero1.value + numero2.value;
-        alert (resultado);
-
+        case '+':
+        function soma(){
+            const resultado = createElement('p');
+                resultado.textContent (numero1 + numero2);
+                resultado.style.color = 'black'
+                document.body.appendChild('p')
+                
+        }
         break;
     
         case '-':
