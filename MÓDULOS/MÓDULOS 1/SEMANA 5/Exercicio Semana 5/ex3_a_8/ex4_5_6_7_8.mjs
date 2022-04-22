d2ce1155bb1ddb7b7ebb809b94fe22b0150f1631
Oseias Lissak <oseiaslissak@hotmail.com>
2022-04-21 23:34:00 -0300

    21/04/22

--- MÓDULOS/MÓDULOS 1/SEMANA 5/Exercicio Semana 5/ex3_a_8/ex4_5_6_7_8.mjs
+++ MÓDULOS/MÓDULOS 1/SEMANA 5/Exercicio Semana 5/ex3_a_8/ex4_5_6_7_8.mjs
@@ -32,19 +32,25 @@ class Pedido {
         });
         //PENDENTE  
         return valorTotal;
+   
+      
     }
 }    
-    
-    // PEDIDO 1 COM 3 PRODUTOS
-    const pedido1 = new Pedido(20220001, 'Oseias');
-    pedido1.adicionarPedido(novoProduto1);
-    pedido1.adicionarPedido(novoProduto2);
-    pedido1.adicionarPedido(novoProduto3);
-   
-    pedido1.listaProdutos.forEach(produto1 => {
-        console.log(`PEDIDO 1: Produto: ${produto1.nome} | Unitário: ${produto1.preco} | Qt: ${produto1.quantidade} | Total= ${produto1.preco * produto1.quantidade}`) 
-    });
-   
+
+// PEDIDO 1 COM 3 PRODUTOS
+const pedido1 = new Pedido(20220001, 'Oseias');
+pedido1.adicionarPedido(novoProduto1);
+pedido1.adicionarPedido(novoProduto2);
+pedido1.adicionarPedido(novoProduto3);
+
+
+
+pedido1.listaProdutos.forEach(produto1 => {
+    console.log(`PEDIDO 1: Produto: ${produto1.nome} | Unitário: ${produto1.preco} | Qt: ${produto1.quantidade} | Total= ${produto1.preco * produto1.quantidade}`) 
+});
+
+
+
       
     // PEDIDO 2 COM 2 PRODUTOS
     const pedido2 = new Pedido(20220002, 'Antonella');
@@ -53,6 +59,8 @@ class Pedido {
     
     pedido2.listaProdutos.forEach(item => {
         console.log(`PEDIDO 2: Produto: ${item.nome} | Unitário: ${item.preco} | Qt: ${item.quantidade} | Total= ${item.preco * item.quantidade}`)});
+
+    
     
     
    
