1de130173bae54148df6f6b15eebab61380e6852
Oseias Lissak <oseiaslissak@hotmail.com>
2022-04-24 23:03:23 -0300

    24/04/22

--- MÓDULOS/MÓDULOS 1/SEMANA 8/Exercicio Semana 8/exercicio-semana-8/src/app/app.component.ts
+++ MÓDULOS/MÓDULOS 1/SEMANA 8/Exercicio Semana 8/exercicio-semana-8/src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 
+interface Menu {
+  titulo: string;
+  imagem: string;
+  path: string
+}
+
+
 @Component({
   selector: 'ngf-root',
   templateUrl: './app.component.html',
@@ -7,4 +14,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'exercicio-semana-8';
+
+  listaMenu: Menu[] = [
+  {
+    titulo: 'Comidas',
+    imagem: '\assets\imagens\Lanches.png',
+    path: 'comidas'
+  },
+  {
+    titulo: 'Bebidas',
+    imagem: '\assets\imagens\bebidas.png',
+    path: 'bebidas'
+  }
+  ]
 }
+
--- MÓDULOS/MÓDULOS 1/SEMANA 8/Exercicio Semana 8/exercicio-semana-8/src/app/app.module.ts
+++ MÓDULOS/MÓDULOS 1/SEMANA 8/Exercicio Semana 8/exercicio-semana-8/src/app/app.module.ts
@@ -2,17 +2,44 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MainComponent } from './components/main/main.component';
+import { ComidasComponent } from './pages/comidas/comidas.component';
+import { BebidasComponent } from './pages/bebidas/bebidas.component';
+import { Route, RouterModule } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { HomeComponent } from './pages/home/home.component';
+
+const ROUTES: Route[] = [
+{ path: '', component: MainComponent },
+
+{ path: 'comidas', component: ComidasComponent },
+
+{ path: 'bebidas', component: BebidasComponent },
+
+{ path: 'about', component: AboutComponent },
+
+{ path: 'main', component: MainComponent },
+
+]
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    MainComponent,
+    ComidasComponent,
+    BebidasComponent,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(ROUTES),
   ],
   providers: [],
   bootstrap: [AppComponent]
